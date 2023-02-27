5ea07a7b44a9000753786f72ec5524954423ed97
mudler <mudler@c3os.io>
2023-02-28 00:18:59 +0100

    :seedling: Bundle should copy into persistent dir
    Signed-off-by: mudler <mudler@c3os.io>

--- examples/airgap/image-bundle/run.sh
+++ examples/airgap/image-bundle/run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-mkdir -p /var/lib/rancher/k3s/agent/images/
-cp ./assets/k3s-airgap-images-amd64.tar /var/lib/rancher/k3s/agent/images/
+# This bundle needs to run after-install as it consumes assets from the LiveCD, which is not accessible at the first boot (there is no live-cd in)
+persistent=$(blkid -L COS_PERSISTENT)
+mkdir /tmp/persistent
+mount $persistent /tmp/persistent
+mkdir -p /usr/local/.state/var-lib-rancher.bind/k3s/agent/images/
+cp ./assets/k3s-airgap-images-amd64.tar /usr/local/.state/var-lib-rancher.bind/k3s/agent/images/
