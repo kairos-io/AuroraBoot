a935ba581aefcf49261475a5b39bbca84194024f
mudler <mudler@c3os.io>
2023-02-28 17:41:34 +0100

    :book: Add an example with container images
    Signed-off-by: mudler <mudler@c3os.io>

--- examples/airgap/build_docker.sh
+++ examples/airgap/build_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-
+IMAGE=quay.io/kairos/kairos-opensuse-leap:v1.6.1-k3sv1.26.1-k3s1
 pushd image-bundle
     docker build -t bundle .
 popd
@@ -13,18 +13,18 @@ pushd data
     docker save bundle -o bundle.tar
 popd
 
+docker pull $IMAGE
+
 docker run -v $PWD/config.yaml:/config.yaml \
              -v $PWD/build:/tmp/auroraboot \
+             -v /var/run/docker.sock:/var/run/docker.sock \
              -v $PWD/data:/tmp/data \
              --rm -ti quay.io/kairos/auroraboot:v0.2.0 \
              --set "disable_http_server=true" \
              --set "disable_netboot=true" \
+             --set "container_image=docker://$IMAGE" \
              --set "iso.data=/tmp/data" \
              --cloud-config /config.yaml \
-             --set "state_dir=/tmp/auroraboot" \
-             --set "artifact_version=v1.6.1-k3sv1.26.1+k3s1" \
-             --set "release_version=v1.6.1" \
-             --set "flavor=fedora" \
-             --set "repository=kairos-io/provider-kairos"
+             --set "state_dir=/tmp/auroraboot"
 
-echo "Custom ISO ready at $PWD/build/iso/kairos.iso.custom.iso"
+echo "Custom ISO ready at $PWD/build/iso/kairos.iso"
