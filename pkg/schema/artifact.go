b51b5ef783e021cc11b4e551df9b3649ec8023fa
Mauro Morales <mauro.morales@spectrocloud.com>
2023-09-29 12:19:19 +0200

    Generate new artifact names
    Signed-off-by: Mauro Morales <mauro.morales@spectrocloud.com>

--- pkg/schema/artifact.go
+++ pkg/schema/artifact.go
@@ -2,41 +2,78 @@ package schema
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 type ReleaseArtifact struct {
 	ArtifactVersion string `yaml:"artifact_version"`
-	ReleaseVersion  string `yaml:"release_version"`
+	Model           string `yaml:"model"`
 	Flavor          string `yaml:"flavor"`
+	Platform        string `yaml:"platform"`
+	ReleaseVersion  string `yaml:"release_version"`
 	Repository      string `yaml:"repository"`
+	Variant         string `yaml:"variant"`
 
 	ContainerImage string `yaml:"container_image"`
 }
 
-func urlGen(repository, releaseVersion, flavor, artifactVersion, artifactType string) string {
-	return fmt.Sprintf("https://github.com/%s/releases/download/%s/%s", repository, releaseVersion, fileName(flavor, artifactVersion, artifactType))
+func (a ReleaseArtifact) FileName() string {
+	if a.Model == "" {
+		a.Model = "generic"
+	}
+	if a.Platform == "" {
+		a.Platform = "amd64"
+	}
+	if a.Variant == "" {
+		if strings.Contains(a.ArtifactVersion, "k3s") {
+			a.Variant = "standard"
+		} else {
+			a.Variant = "core"
+		}
+
+	}
+
+	var re = regexp.MustCompile(`v(?P<major>\d+)\.(?P<minor>\d+).+`)
+	match := re.FindStringSubmatch(a.ReleaseVersion)
+	result := make(map[string]string)
+	for i, name := range re.SubexpNames() {
+		if i != 0 && name != "" {
+			result[name] = match[i]
+		}
+	}
+
+	major, _ := strconv.Atoi(result["major"])
+	minor, _ := strconv.Atoi(result["minor"])
+
+	if major < 2 || (major == 2 && minor < 4) {
+		return fmt.Sprintf("kairos-%s-%s", a.Flavor, a.ArtifactVersion)
+	}
+
+	return fmt.Sprintf("kairos-%s-%s-%s-generic-%s", a.Variant, a.Flavor, a.Platform, a.ArtifactVersion)
 }
 
-func fileName(flavor, artifactVersion, artifactType string) string {
-	return fmt.Sprintf("kairos-%s-%s%s", flavor, artifactVersion, artifactType)
+func (a ReleaseArtifact) urlGen(ext string) string {
+	return fmt.Sprintf("https://github.com/%s/releases/download/%s/%s%s", a.Repository, a.ReleaseVersion, a.FileName(), ext)
 }
 
 func (a ReleaseArtifact) NetbootArtifacts() []string {
-
 	return []string{a.InitrdURL()}
 }
+
 func (a ReleaseArtifact) ISOUrl() string {
-	return urlGen(a.Repository, a.ReleaseVersion, a.Flavor, a.ArtifactVersion, ".iso")
+	return a.urlGen(".iso")
 }
 
 func (a ReleaseArtifact) InitrdURL() string {
-	return urlGen(a.Repository, a.ReleaseVersion, a.Flavor, a.ArtifactVersion, "-initrd")
+	return a.urlGen("-initrd")
 }
 
 func (a ReleaseArtifact) KernelURL() string {
-	return urlGen(a.Repository, a.ReleaseVersion, a.Flavor, a.ArtifactVersion, "-kernel")
+	return a.urlGen("-kernel")
 }
 
 func (a ReleaseArtifact) SquashFSURL() string {
-	return urlGen(a.Repository, a.ReleaseVersion, a.Flavor, a.ArtifactVersion, ".squashfs")
+	return a.urlGen(".squashfs")
 }
