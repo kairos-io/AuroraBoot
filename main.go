2166c8674f99211bfa82130b88ed55ec5acf4b74
Dimitris Karakasilis <dimitris@karakasilis.me>
2024-10-23 18:03:09 +0300

    Extract build-iso command to a separate file
    and bring all the flags from enki (TODO: Parse them and create a config
    to be passed to the Deployer)
    
    Signed-off-by: Dimitris Karakasilis <dimitris@karakasilis.me>

--- main.go
+++ main.go
@@ -20,51 +20,12 @@ func main() {
 
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
-	buildISOCmd := cli.Command{
-		Name:  "iso",
-		Usage: "build an iso artifact from a container image or github release",
-		Action: func(ctx *cli.Context) error {
-			// TODO: Read these from command line args and build one config to by used
-			// by all actions
-			c, r, err := cmd.ReadConfig(ctx.Args().First(), ctx.String("cloud-config"), ctx.StringSlice("set"))
-			if err != nil {
-				return err
-			}
-
-			// TODO: Move to a RegisterBuildIso function
-			d := deployer.NewDeployer(*c, *r)
-			for _, step := range []func() error{
-				// TODO: Add more steps
-				d.StepPrepNetbootDir,
-				d.StepPrepTmpRootDir,
-				d.StepPrepDestDir,
-			} {
-				if err := step(); err != nil {
-					return err
-				}
-			}
-
-			if err := d.Run(ctx.Context); err != nil {
-				return err
-			}
-
-			return d.CollectErrors()
-		},
-	}
-
-	buildCmd := cli.Command{
-		Name:        "build",
-		Aliases:     []string{"b"},
-		Usage:       "build an artifact from a container image or github release",
-		Subcommands: []*cli.Command{&buildISOCmd},
-	}
-
 	app := &cli.App{
 		Name:     "AuroraBoot",
 		Version:  version,
 		Authors:  []*cli.Author{{Name: "Kairos authors", Email: "members@kairos.io"}},
 		Usage:    "auroraboot",
-		Commands: []*cli.Command{&buildCmd},
+		Commands: []*cli.Command{&cmd.BuildISOCmd},
 		Flags: []cli.Flag{
 			&cli.StringSliceFlag{
 				Name: "set",
@@ -89,9 +50,6 @@ func main() {
 			if err != nil {
 				return err
 			}
-
-			fmt.Printf("c = %#v\n", c)
-			fmt.Printf("r = %#v\n", r)
 
 			if err := deployer.Start(c, r); err != nil {
 				return err
