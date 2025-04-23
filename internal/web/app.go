16c421cd47f97f2fced24be0ce71726a8afde7e5
Dimitris Karakasilis <dimitris@karakasilis.me>
2025-04-23 13:18:30 +0300

    Extract handlers to separate functions
    and add docker instructions for local development
    
    Signed-off-by: Dimitris Karakasilis <dimitris@karakasilis.me>

--- internal/web/app.go
+++ internal/web/app.go
@@ -19,6 +19,10 @@ import (
 //go:embed app
 var staticFiles embed.FS
 
+var mu sync.Mutex
+var jobsData = map[string]JobData{} // Store the last submitted form data
+var artifactDir string
+
 //go:embed assets
 var assets embed.FS
 
@@ -45,7 +49,8 @@ type JobData struct {
 	Version                string `json:"version"`
 }
 
-func App(listenAddr, artifactDir string) error {
+func App(listenAddr, outDir string) error {
+	artifactDir = outDir
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
@@ -56,156 +61,11 @@ func App(listenAddr, artifactDir string) error {
 	assetHandler := http.FileServer(getFileSystem(false))
 	e.GET("/*", echo.WrapHandler(assetHandler))
 
-	// Store the last submitted form data
-	jobsData := map[string]JobData{}
-	var mu sync.Mutex
-
 	// Handle form submission
-	e.POST("/start", func(c echo.Context) error {
-		mu.Lock()
-		defer mu.Unlock()
-
-		image := c.FormValue("base_image")
-		if image == "byoi" {
-			image = c.FormValue("byoi_image")
-		}
-
-		variant := c.FormValue("variant")
-
-		kubernetesDistribution := c.FormValue("kubernetes_distribution")
-		kubernetesVersion := c.FormValue("kubernetes_version")
-		if variant == "core" {
-			kubernetesDistribution = ""
-			kubernetesVersion = ""
-		}
-
-		// Collect job data
-		job := JobData{
-			Variant:                variant,
-			Model:                  c.FormValue("model"),
-			TrustedBoot:            c.FormValue("trusted_boot") == "true",
-			KubernetesDistribution: kubernetesDistribution,
-			KubernetesVersion:      kubernetesVersion,
-			Image:                  image,
-			Version:                c.FormValue("version"),
-		}
-
-		id, err := uuid.NewV4()
-		if err != nil {
-			return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to generate UUID"})
-		}
-
-		jobsData[id.String()] = job
-
-		return c.JSON(http.StatusOK, map[string]string{"uuid": id.String()})
-	})
+	e.POST("/start", buildHandler)
 
 	// Handle WebSocket connection
-	e.GET("/ws/:uuid", func(c echo.Context) error {
-		websocket.Handler(func(ws *websocket.Conn) {
-			mu.Lock()
-			uuid := c.Param("uuid")
-			jobData, exists := jobsData[uuid]
-			mu.Unlock()
-			if !exists {
-				websocket.Message.Send(ws, "Job not found")
-				return
-			}
-
-			defer ws.Close()
-
-			// Log the start of the process
-			websocket.Message.Send(ws, fmt.Sprintf("Starting process with data: %+v\n", jobData))
-
-			tempdir, err := os.MkdirTemp("", "build")
-			if err != nil {
-				websocket.Message.Send(ws, fmt.Sprintf("Failed to create temp dir: %v", err))
-				return
-			}
-
-			defer os.RemoveAll(tempdir)
-
-			if err := prepareDockerfile(jobData, tempdir); err != nil {
-				websocket.Message.Send(ws, fmt.Sprintf("Failed to prepare image: %v", err))
-				return
-			}
-
-			websocket.Message.Send(ws, "Building container image...")
-
-			err = runBashProcessWithOutput(ws,
-				buildOCI(
-					tempdir,
-					"my-image",
-				))
-
-			if err != nil {
-				websocket.Message.Send(ws, fmt.Sprintf("Failed to build image: %v", err))
-				return
-			}
-
-			// Create the output dir for the job
-			jobOutputDir := filepath.Join(artifactDir, uuid)
-			os.MkdirAll(jobOutputDir, os.ModePerm)
-
-			websocket.Message.Send(ws, "Generating tarball...")
-
-			err = runBashProcessWithOutput(
-				ws,
-				saveOCI(filepath.Join(jobOutputDir, "image.tar"), "my-image"),
-			)
-			if err != nil {
-				websocket.Message.Send(ws, fmt.Sprintf("Failed to save image: %v", err))
-				return
-			}
-
-			websocket.Message.Send(ws, "Generating raw image...")
-			err = runBashProcessWithOutput(
-				ws,
-				buildRawDisk("my-image", jobOutputDir),
-			)
-			if err != nil {
-				websocket.Message.Send(ws, fmt.Sprintf("Failed to save image: %v", err))
-				return
-			}
-
-			websocket.Message.Send(ws, "Generating ISO...")
-			err = runBashProcessWithOutput(
-				ws,
-				buildISO("my-image", jobOutputDir, "custom-kairos"),
-			)
-			if err != nil {
-				websocket.Message.Send(ws, fmt.Sprintf("Failed to save image: %v", err))
-				return
-			}
-
-			type Link struct {
-				Name string `json:"name"`
-				URL  string `json:"url"`
-			}
-
-			rawImage, err := searchFileByExtensionInDirectory(jobOutputDir, ".raw")
-			if err != nil {
-				websocket.Message.Send(ws, fmt.Sprintf("Failed to find raw disk image: %v", err))
-				return
-			}
-
-			websocket.Message.Send(ws, "Generating download links...")
-			links := []Link{
-				{Name: "Container image", URL: "/artifacts/" + uuid + "/image.tar"},
-				{Name: "Raw disk image", URL: "/artifacts/" + uuid + "/" + filepath.Base(rawImage)},
-				{Name: "ISO image", URL: "/artifacts/" + uuid + "/custom-kairos.iso"},
-			}
-
-			dat, err := json.Marshal(links)
-			if err != nil {
-				websocket.Message.Send(ws, fmt.Sprintf("Failed to marshal links: %v", err))
-				return
-			}
-
-			websocket.Message.Send(ws, string(dat))
-		}).ServeHTTP(c.Response(), c.Request())
-		return nil
-	})
+	e.GET("/ws/:uuid", webSocketHandler)
 
 	// Serve static artifact files
 	e.Static("/artifacts", artifactDir)
@@ -243,4 +103,149 @@ func searchFileByExtensionInDirectory(artifactDir, ext string) (string, error) {
 	}
 
 	return file, nil
+}
+
+func buildHandler(c echo.Context) error {
+	mu.Lock()
+	defer mu.Unlock()
+
+	image := c.FormValue("base_image")
+	if image == "byoi" {
+		image = c.FormValue("byoi_image")
+	}
+
+	variant := c.FormValue("variant")
+
+	kubernetesDistribution := c.FormValue("kubernetes_distribution")
+	kubernetesVersion := c.FormValue("kubernetes_version")
+	if variant == "core" {
+		kubernetesDistribution = ""
+		kubernetesVersion = ""
+	}
+
+	// Collect job data
+	job := JobData{
+		Variant:                variant,
+		Model:                  c.FormValue("model"),
+		TrustedBoot:            c.FormValue("trusted_boot") == "true",
+		KubernetesDistribution: kubernetesDistribution,
+		KubernetesVersion:      kubernetesVersion,
+		Image:                  image,
+		Version:                c.FormValue("version"),
+	}
+
+	id, err := uuid.NewV4()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to generate UUID"})
+	}
+
+	jobsData[id.String()] = job
+
+	return c.JSON(http.StatusOK, map[string]string{"uuid": id.String()})
+}
+
+func webSocketHandler(c echo.Context) error {
+	websocket.Handler(func(ws *websocket.Conn) {
+		mu.Lock()
+		uuid := c.Param("uuid")
+		jobData, exists := jobsData[uuid]
+		mu.Unlock()
+		if !exists {
+			websocket.Message.Send(ws, "Job not found")
+			return
+		}
+
+		defer ws.Close()
+
+		// Log the start of the process
+		websocket.Message.Send(ws, fmt.Sprintf("Starting process with data: %+v\n", jobData))
+
+		tempdir, err := os.MkdirTemp("", "build")
+		if err != nil {
+			websocket.Message.Send(ws, fmt.Sprintf("Failed to create temp dir: %v", err))
+			return
+		}
+
+		defer os.RemoveAll(tempdir)
+
+		if err := prepareDockerfile(jobData, tempdir); err != nil {
+			websocket.Message.Send(ws, fmt.Sprintf("Failed to prepare image: %v", err))
+			return
+		}
+
+		websocket.Message.Send(ws, "Building container image...")
+
+		err = runBashProcessWithOutput(ws,
+			buildOCI(
+				tempdir,
+				"my-image",
+			))
+
+		if err != nil {
+			websocket.Message.Send(ws, fmt.Sprintf("Failed to build image: %v", err))
+			return
+		}
+
+		// Create the output dir for the job
+		jobOutputDir := filepath.Join(artifactDir, uuid)
+		os.MkdirAll(jobOutputDir, os.ModePerm)
+
+		websocket.Message.Send(ws, "Generating tarball...")
+
+		err = runBashProcessWithOutput(
+			ws,
+			saveOCI(filepath.Join(jobOutputDir, "image.tar"), "my-image"),
+		)
+		if err != nil {
+			websocket.Message.Send(ws, fmt.Sprintf("Failed to save image: %v", err))
+			return
+		}
+
+		websocket.Message.Send(ws, "Generating raw image...")
+		err = runBashProcessWithOutput(
+			ws,
+			buildRawDisk("my-image", jobOutputDir),
+		)
+		if err != nil {
+			websocket.Message.Send(ws, fmt.Sprintf("Failed to save image: %v", err))
+			return
+		}
+
+		websocket.Message.Send(ws, "Generating ISO...")
+		err = runBashProcessWithOutput(
+			ws,
+			buildISO("my-image", jobOutputDir, "custom-kairos"),
+		)
+		if err != nil {
+			websocket.Message.Send(ws, fmt.Sprintf("Failed to save image: %v", err))
+			return
+		}
+
+		type Link struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		}
+
+		rawImage, err := searchFileByExtensionInDirectory(jobOutputDir, ".raw")
+		if err != nil {
+			websocket.Message.Send(ws, fmt.Sprintf("Failed to find raw disk image: %v", err))
+			return
+		}
+
+		websocket.Message.Send(ws, "Generating download links...")
+		links := []Link{
+			{Name: "Container image", URL: "/artifacts/" + uuid + "/image.tar"},
+			{Name: "Raw disk image", URL: "/artifacts/" + uuid + "/" + filepath.Base(rawImage)},
+			{Name: "ISO image", URL: "/artifacts/" + uuid + "/custom-kairos.iso"},
+		}
+
+		dat, err := json.Marshal(links)
+		if err != nil {
+			websocket.Message.Send(ws, fmt.Sprintf("Failed to marshal links: %v", err))
+			return
+		}
+
+		websocket.Message.Send(ws, string(dat))
+	}).ServeHTTP(c.Response(), c.Request())
+	return nil
 }
