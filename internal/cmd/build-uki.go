527d68a2c3ee6bbfef10f83bf4ba9d562e04358c
Itxaka <itxaka@kairos.io>
2026-01-21 10:08:04 +0100

    Update internal/cmd/build-uki.go
    Co-authored-by: Dimitris Karakasilis <dimitris@karakasilis.me>

--- internal/cmd/build-uki.go
+++ internal/cmd/build-uki.go
@@ -810,6 +810,7 @@ func createConfFiles(sourceDir, cmdline, title, finalEfiName, version, profile s
 
 	// You can add entries into the config files, they will be ignored by systemd-boot
 	// So we store the cmdline in a key cmdline for easy tracking of what was added to the uki cmdline
+    // https://github.com/systemd/systemd/blob/main/src/boot/boot.c#L77
 	configData := fmt.Sprintf("title %s\nsort-key %s-%s\nuki /EFI/kairos/%s.efi\nprofile %s\n", title, finalEfiName, profile, finalEfiName, profile)
 
 	if includeVersion {
